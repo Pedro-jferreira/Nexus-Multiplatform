bcf15b94c47f85898e73791d2890a3e871a406de
Pedro-jferreira <pedrohenriquedejesusferreira13@gmail.com>
2025-10-28 09:35:45 -0300

    :recycle: refac(dependencies) adição de um cliente http dio global

--- lib/config/dependencies.dart
+++ lib/config/dependencies.dart
@@ -1,14 +1,20 @@
 
 
+import 'package:dio/dio.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import 'dio_config.dart';
+
 List<SingleChildWidget> providers(SharedPreferences prefs,GoRouter router) {
   return [
     Provider<SharedPreferences>.value(value: prefs),
     Provider<GoRouter>.value(value: router),
+    Provider<Dio>(
+      create: (context)=> DioConfig.createDio(router: context.read(), prefs: context.read() ),
+    ),
     ..._servicesData,
     ..._repositoriesData,
     ..._viewModelsProviders,
