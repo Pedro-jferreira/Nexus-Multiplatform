03931f4c4bd815cfa51a00ca77a2259296972526
Pedro-jferreira <pedrohenriquedejesusferreira13@gmail.com>
2025-10-28 09:38:21 -0300

    :sparkles: feat(ThemeMobile) adição de thema statico no mobile

---
+++ lib/ui/core/theme/theme_mobile.dart
@@ -0,0 +1,29 @@
+ import 'dart:ui';
+
+const primaryMain = Color.fromRGBO(21, 101, 192, 1);
+const primaryEmphasis = Color.fromRGBO(121, 172, 255, 1);
+const primarySubtle = Color.fromRGBO(214, 227, 255, 1);
+
+const secondaryMain = Color.fromRGBO(0, 137, 123, 1);
+const secondaryEmphasis = Color.fromRGBO(82, 188, 172, 1);
+const secondarySubtle = Color.fromRGBO(198, 248, 234, 1);
+
+const backGround = Color.fromRGBO(229, 229, 229, 1);
+const surface = Color.fromRGBO(255, 255, 255, 1);
+
+const text = Color.fromRGBO(33, 33, 33, 1);
+const textPlaceholder = Color.fromRGBO(173, 170, 170, 1);
+
+const success = Color.fromRGBO(46, 125, 50, 1);
+const error = Color.fromRGBO(193, 18, 31, 1);
+
+
+
+
+
+
+
+
+
+
+
