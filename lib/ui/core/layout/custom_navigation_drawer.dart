32400c082e00fafbf37d9c171a235672cf1896e5
Pedro-jferreira <pedrohenriquedejesusferreira13@gmail.com>
2025-11-11 13:29:29 -0300

    :sparkles: feat(navigation_drawer) componente de navegação criado e incluso no shell

--- lib/ui/core/layout/custom_navigation_drawer.dart
+++ lib/ui/core/layout/custom_navigation_drawer.dart
@@ -3,23 +3,226 @@ import 'package:nexus_multiplatform/data/repositories/auth_repository.dart';
 import 'package:provider/provider.dart';
 
 class CustomNavigationDrawer extends StatelessWidget {
-  const CustomNavigationDrawer({super.key});
+  final int currentIndex;
+  final ValueChanged<int> onItemSelected;
+
+  const CustomNavigationDrawer({
+    super.key,
+    required this.currentIndex,
+    required this.onItemSelected,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.yellow,
-      width: 300,
-      height: double.infinity,
-      child: Center(
-        child: ElevatedButton(
-          onPressed: () async {
-            final repository = context.read<AuthRepository>();
-            await repository.logout();
+    final theme = Theme.of(context);
+    final colorScheme = theme.colorScheme;
+
+    final items = [
+      _DrawerItem(title: 'Dashboard', icon: Icons.dashboard_outlined),
+      _DrawerItem(title: 'Foragidos', icon: Icons.person_search_outlined),
+      _DrawerItem(title: 'Usuários', icon: Icons.groups_outlined),
+      _DrawerItem(title: 'Chats', icon: Icons.chat_outlined),
+      _DrawerItem(title: 'Incidentes', icon: Icons.report_outlined),
+      _DrawerItem(
+        title: 'Contato de Emergência',
+        icon: Icons.contact_emergency_outlined,
+      ),
+      _DrawerItem(title: 'Perfil', icon: Icons.person, subtitle: 'Admin'),
+    ];
+
+    return Drawer(
+      backgroundColor: colorScheme.surfaceContainerLowest,
+      elevation: 5,
+      shadowColor: colorScheme.shadow,
+      child: SafeArea(
+        child: LayoutBuilder(
+          builder: (context, constraints) {
+            return SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minHeight: constraints.maxHeight,
+                ),
+                child: IntrinsicHeight(
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 24),
+                          child: Image.asset(
+                            theme.brightness == Brightness.dark
+                                ? 'assets/logos/Logo_sigla_branco.png'
+                                : 'assets/logos/Logo_sigla_horizontal.png',
+                            height: 45,
+                          ),
+                        ),
+
+                        const Divider(thickness: 1),
+                        ...items.asMap().entries.map((entry) {
+                          final index = entry.key;
+                          final item = entry.value;
+
+                          if (index == items.length - 1) {
+                            return const SizedBox.shrink();
+                          }
+                          final selected = currentIndex == index;
+                          return _DrawerTile(
+                            item: item,
+                            selected: selected,
+                            onTap: () => onItemSelected(index),
+                          );
+                        }),
+                        const Divider(thickness: 1),
+                        Spacer(),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 8, bottom: 16),
+                          child: InkWell(
+                            borderRadius: BorderRadius.circular(100),
+                            hoverColor: colorScheme.secondary..withValues(alpha: 0.15),
+                            onTap: () => onItemSelected(items.length - 1),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: currentIndex == (items.length - 1)
+                                    ? colorScheme.secondaryContainer.withValues(
+                                        alpha: 0.3,
+                                      )
+                                    : null,
+                                borderRadius: BorderRadius.circular(100),
+                              ),
+                              padding: EdgeInsets.all(5),
+                              child: Row(
+                                spacing: 5,
+                                children: [
+                                  SizedBox(
+                                    width: 47,
+                                    height: 47,
+                                    child: CircleAvatar(
+                                      backgroundColor: colorScheme
+                                          .primaryContainer
+                                          .withValues(alpha: 0.6),
+                                      child: const Icon(Icons.person, size: 20),
+                                    ),
+                                  ),
+                                  Column(
+                                    children: [
+                                      Text(
+                                        items.last.title,
+                                        style: theme.textTheme.bodyLarge
+                                            ?.copyWith(
+                                              color:
+                                                  currentIndex ==
+                                                      items.length - 1
+                                                  ? colorScheme.onSurface
+                                                  : colorScheme.secondary,
+                                            ),
+                                      ),
+                                      if (items.last.subtitle != null)
+                                        Text(
+                                          items.last.subtitle!,
+                                          style: theme.textTheme.bodySmall
+                                              ?.copyWith(
+                                                color:
+                                                    currentIndex ==
+                                                        items.length - 1
+                                                    ? colorScheme
+                                                          .onSurfaceVariant
+                                                    : colorScheme.secondary,
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                        ),
+                                    ],
+                                  ),
+                                  Spacer(),
+                                  FilledButton.icon(
+                                    icon: const Icon(Icons.logout, size: 24),
+                                    onPressed: () async {
+                                      final repository = context
+                                          .read<AuthRepository>();
+                                      await repository.logout();
+                                    },
+                                    label: Text('Sair'),
+                                    style: FilledButton.styleFrom(
+                                      padding: EdgeInsets.all(16),
+                                      backgroundColor:
+                                          colorScheme.errorContainer,
+                                      foregroundColor:
+                                          colorScheme.onErrorContainer,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            );
           },
-          child: Text('logout'),
         ),
       ),
     );
   }
+}
+
+/// 🔹 Widget individual do item
+class _DrawerTile extends StatelessWidget {
+  final _DrawerItem item;
+  final bool selected;
+  final VoidCallback onTap;
+
+  const _DrawerTile({
+    required this.item,
+    required this.selected,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return ListTile(
+      leading: Icon(
+        item.icon,
+        color: selected
+            ? colorScheme.onSecondaryContainer
+            : colorScheme.onSurfaceVariant,
+      ),
+      title: Text(
+        item.title,
+        style: Theme.of(context).textTheme.labelLarge?.copyWith(
+          color: selected
+              ? colorScheme.onSecondaryContainer
+              : colorScheme.onSurfaceVariant,
+        ),
+      ),
+      onTap: onTap,
+      subtitle: item.subtitle == null
+          ? null
+          : Text(
+              item.subtitle!,
+              style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                color: selected
+                    ? colorScheme.onSecondaryContainer
+                    : colorScheme.onSurfaceVariant,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+      selected: selected,
+      selectedTileColor: colorScheme.secondaryContainer,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(100)),
+      hoverColor: colorScheme.secondary.withValues(alpha: 0.15),
+    );
+  }
+}
+
+class _DrawerItem {
+  final String title;
+  final IconData icon;
+  final String? subtitle;
+
+  _DrawerItem({required this.title, required this.icon, this.subtitle});
 }
--- lib/ui/features/shell_web/shell_app.dart
+++ lib/ui/features/shell_web/shell_app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:nexus_multiplatform/ui/core/layout/custom_app_bar.dart';
 
 import '../../core/layout/custom_navigation_drawer.dart';
 
@@ -14,12 +15,22 @@ class ShellApp extends StatefulWidget {
 class _ShellAppState extends State<ShellApp> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: [
-          CustomNavigationDrawer(),
-          Expanded(child: widget.child)
-        ],
+    return Scaffold(
+      body: Center(
+        child: Row(
+          children: [
+            CustomNavigationDrawer(
+              onItemSelected: widget.child.goBranch,
+              currentIndex: widget.child.currentIndex,
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(60),
+                child: widget.child,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
