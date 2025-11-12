4fb8e3fdedd587b1cf779875fe0cd086a966dde9
Pedro-jferreira <pedrohenriquedejesusferreira13@gmail.com>
2025-11-12 17:29:08 -0300

    :sparkles: feat(emergency_contacts_validator) adição de validadores para feature contatos de emergência

---
+++ lib/domain/validators/emergency_validator.dart
@@ -0,0 +1,34 @@
+import 'package:lucid_validation/lucid_validation.dart';
+import 'package:nexus_multiplatform/domain/models/enums/api_enums.dart';
+
+class ContactRequestDTO {
+  String nome;
+  String phone;
+  ServiceType? type;
+
+  ContactRequestDTO({
+    required this.nome,
+    required this.phone,
+    required this.type
+  });
+
+  factory ContactRequestDTO.empty() => ContactRequestDTO(nome: '', phone: '', type: null);
+
+  setNome(nome) => this.nome = nome;
+  setPhone(phone) => this.phone = phone;
+  setType(type) => this.type = type;
+}
+class ContactValidador extends LucidValidator<ContactRequestDTO> {
+  ContactValidador() {
+
+    ruleFor((param) => param.nome, key: 'nome')
+        .notEmpty(message: 'Informe um nome.');
+
+    ruleFor((param) => param.phone, key: 'phone')
+        .notEmpty(message: 'Informe um número');
+
+    ruleFor((param) => param.type?.name, key: 'type')
+        .notEmpty(message: 'Informe um nome.');
+
+  }
+}
