676a0485a199710587278e2bd9e2c6ef623f7202
Pedro-jferreira <pedrohenriquedejesusferreira13@gmail.com>
2025-11-24 21:18:20 -0300

    :recycle: refactor(data) adição de serviço de usuários

---
+++ lib/data/repositories/user_repository.dart
@@ -0,0 +1,24 @@
+import 'package:Nexus/domain/models/requests/gen_models.dart';
+import 'package:Nexus/domain/models/responses/gen_models.dart';
+import 'package:result_dart/result_dart.dart';
+
+abstract class UserRepository{
+  AsyncResult<UserResponse> create({
+    required CreateUserRequest model,
+    required FileRequest file,
+  });
+
+  AsyncResult<UserResponse> getById(int id);
+
+  AsyncResult<PageResponse<UserResponse>> list({
+    required UserFilter filter
+  });
+
+  AsyncResult<UserResponse> update({
+    required int id,
+    required UpdateUserRequest model,
+    required FileRequest file,
+  });
+
+  AsyncResult<void> delete(int id);
+}
