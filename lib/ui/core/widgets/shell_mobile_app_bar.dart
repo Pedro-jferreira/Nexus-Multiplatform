5e0cc1557e85518b0a3052064b234c0cbdcf6b80
Davi Antonio <adavi9920@gmail.com>
2025-11-25 17:19:15 -0300

    feat: avançando na implementação da appbar do shell mobile.

--- lib/ui/core/widgets/shell_mobile_app_bar.dart
+++ lib/ui/core/widgets/shell_mobile_app_bar.dart
@@ -2,14 +2,14 @@ import 'package:Nexus/ui/core/widgets/custom_circle_avatar.dart';
 import 'package:flutter/material.dart';
 import 'package:Nexus/ui/core/theme/theme_mobile.dart';
 
-class ShellMobileAppBar extends StatefulWidget implements PreferredSizeWidget{
+class ShellMobileAppBar extends StatefulWidget implements PreferredSizeWidget {
   const ShellMobileAppBar({super.key});
 
   @override
   State<ShellMobileAppBar> createState() => _ShellMobileAppBarState();
 
   @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+  Size get preferredSize => const Size.fromHeight(74);
 }
 
 class _ShellMobileAppBarState extends State<ShellMobileAppBar> {
@@ -17,10 +17,27 @@ class _ShellMobileAppBarState extends State<ShellMobileAppBar> {
   Widget build(BuildContext context) {
     return AppBar(
       backgroundColor: context.colors.primaryMain,
-      leading: Image.asset('assets/logos/Logo_sigla_branco.png'),
-      actionsIconTheme: IconThemeData(color: context.colors.surface),
+      leadingWidth: 134,
+      leading: Padding(
+        padding: const EdgeInsets.only(left: 50, top: 11),
+        child: SizedBox(
+          width: 104,
+          height: 52,
+          child: Image.asset(
+            'assets/logos/Logo_sigla_branco.png',
+            fit: BoxFit.contain,
+          ),
+        ),
+      ),
       actions: [
-        CustomCircleAvatar(iconData: Icons.person,),
+        Padding(
+          padding: const EdgeInsets.only(right: 50, top: 11),
+          child: CustomCircleAvatar(
+            iconData: Icons.person,
+            height: 44,
+            width: 44,
+          ),
+        ),
       ],
     );
   }
